c225480494b2cffd2999530263d4567d5417e675
SwampTG <adriansoareslps@gmail.com>
2022-11-20 14:46:46 -0300

    DB Model for investigation

---
+++ models/investigation.ts
@@ -0,0 +1,52 @@
+import {model, Model, Schema, Types} from 'mongoose';
+
+export interface IInvestElem {
+    _id: Types.ObjectId;
+    name: string;
+    descript: string;
+    type:string;
+    adjList: Types.ObjectId[];
+}
+
+export type InvestElemModel = Model<IInvestElem> ;
+
+export const investElemSchema = new Schema<IInvestElem, InvestElemModel>({
+    adjList: [{ type: Schema.Types.ObjectId, ref: 'InvestElem'}],
+    name: {type: String, required: true},
+    descript: {type: String, required: true},
+    type: {type: String, required: true, enum: ['Person', 'Local', 'Object']},
+});
+
+export const InvestElem = model<IInvestElem, InvestElemModel>('InvestElem', investElemSchema);
+
+export interface IInvestigation {
+    vertexes: IInvestElem[];
+    isCyclic: boolean;
+    isDirected: boolean;
+    start: number;
+    hasTopOrder: boolean;
+    isBipartite: boolean;
+    type: string;
+}
+
+export type IInvestigationDocProps = {
+    vertexes: Types.DocumentArray<IInvestElem>;
+} 
+
+export type InvestigationModelType = Model<IInvestigation, {}, IInvestigationDocProps>;
+
+export const invetigationSchema = new Schema<IInvestigation, InvestigationModelType>({
+    vertexes: [new Schema<IInvestElem>({
+        adjList: [{ type: Schema.Types.ObjectId, ref: 'InvestElem'}],
+        name: {type: String, required: true},
+        descript: {type: String, required: true},
+        type: {type: String, required: true, enum: ['person', 'local', 'object']},
+    })],
+    isDirected: { type: Boolean },
+    start: { type: Number, required: true},
+    hasTopOrder: { type: Boolean },
+    isBipartite: { type: Boolean },
+    type: { type: String, enum: ['directed', 'undirected'] },
+});
+
+export const InvestigationModel = model<IInvestigation, InvestigationModelType>('Investigation', invetigationSchema);
