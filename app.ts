88f5812b770ae497751004fbfea08bae0ac2562f
SwampTG <adriansoareslps@gmail.com>
2022-11-20 09:55:54 -0300

    Mongodb connection and config
    Co-authored-by: Paulo Vitor <PauloAbiAcl@users.noreply.github.com>

---
+++ app.ts
@@ -0,0 +1,25 @@
+require('dotenv').config({path: './.config.env'});
+import configDb from './config/configdb';
+
+
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+configDb();
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
