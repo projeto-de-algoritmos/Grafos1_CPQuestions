6de249e608f542d93ebcf4453ebb1612222e93fe
SwampTG <adriansoareslps@gmail.com>
2022-11-21 11:21:33 -0300

    Revert "Merge pull request #2 from projeto-de-algoritmos/express-development"
    This reverts commit 305f8a6f2040ec95bc4e2b7c0547db93c06dd12d, reversing
    changes made to e8222d0ebe0f1ba89eec84fe6c6d4e4a3d73c86a.

--- routes/edge.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import EdgeController from '../controllers/edge';
-
-const router = express.Router();
-
-router.post('/:id/add/', EdgeController.addEdge);
-// router.post('/remove/', EdgeController.removeEdge);
-
-export default router;
--- utils/types/edge.ts
+++
@@ -1,7 +0,0 @@
-import Vertex from '../interfaces/vertex';
-
-export type Edge = {
-  u1: Vertex;
-  u2: Vertex;
-}
-
